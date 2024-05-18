8a949838cf385d1502efc93fe8f397e08a6e14ea
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2024-05-18 12:46:20 -0500

    load terrain sprite sheet; blit floor blocks

--- main.py
+++ main.py
@@ -43,6 +43,15 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
     return sprites
 
 
+def load_block(size):
+    path = join("assets", "Terrain", "Terrain.png")
+    image = pygame.image.load(path).convert_alpha()
+    surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
+    rect = pygame.Rect(96, 0, size, size)
+    surface.blit(image, (0, 0), rect)
+    return pygame.transform.scale2x(surface)
+
+
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
@@ -51,7 +60,7 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self, x, y, width, height):
         # initialize the super class
-        pygame.sprite.Sprite.__init__(self)
+        super().__init__()
         self.rect = pygame.Rect(x, y, width, height)
 
         # decompose motion into x and y components
@@ -119,6 +128,27 @@ class Player(pygame.sprite.Sprite):
         window.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
+class Object(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, name=None):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+        self.width = width
+        self.height = height
+        self.name = name
+
+    def draw(self, window):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
+        block = load_block(size)
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
+
+
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name))
     _, _, width, height = image.get_rect()
@@ -133,9 +163,12 @@ def get_background(name):
     return tiles, image
 
 
-def draw(window, background, bg_image, player):
+def draw(window, background, bg_image, player, objects):
     for tile in background:
         window.blit(bg_image, tile)
+
+    for obj in objects:
+        obj.draw(window)
 
     player.draw(window)
 
@@ -158,7 +191,19 @@ def handle_move(player):
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Blue.png")
+
+    block_size = 96
+
     player = Player(100, 100, 50, 50)
+
+    # floor blocks extend 3x the width of the game window
+    floor = [
+        Block(i * block_size, HEIGHT - block_size, block_size)
+        for i in range(-WIDTH // block_size, (WIDTH * 2) // block_size)
+    ]
+
+    # accumulate all objects to pass to draw function
+    objects = floor
 
     running = True
     while running:
@@ -172,7 +217,7 @@ def main(window):
 
         handle_move(player)  # process player-level input
         player.loop(FPS)  # update player
-        draw(window, background, bg_image, player)  # draw new frame
+        draw(window, background, bg_image, player, objects)  # draw new frame
 
     pygame.quit()
     quit()
