b55687fba9e06e58befb550831f9ebd457e7e3c7
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2024-05-17 20:09:16 -0500

    load and tile background

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os.path import join
 import pygame
 
 pygame.init()
@@ -10,8 +11,30 @@ pygame.display.set_caption("Legend of Ninja Frog")
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            top_left_corner = (i * width, j * height)
+            tiles.append(top_left_corner)
+
+    return tiles, image
+
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
+
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Blue.png")
 
     running = True
     while running:
@@ -21,6 +44,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 running = False
                 break
+
+        draw(window, background, bg_image)
 
     pygame.quit()
     quit()
