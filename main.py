f233865c22282c65cc0201f14d8052ba6e879bd7
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2024-05-18 12:04:48 -0500

    load and process sprite sheets; animate at run and idle (gravity turned off)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from os.path import join
+from os import listdir
+from os.path import isfile, join
 import pygame
 
 pygame.init()
@@ -11,9 +12,42 @@ pygame.display.set_caption("Legend of Ninja Frog")
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+def flip(sprites):
+    # flip all spirtes in list around the y-axis
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    image_files = [f for f in listdir(path) if isfile(join(path, f))]
+
+    sprites = {}
+
+    for image_file in image_files:
+        sprite_sheet = pygame.image.load(join(path, image_file)).convert_alpha()
+
+        sprites_list = []
+
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites_list.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            sprites[image_file.replace(".png", "") + "_right"] = sprites_list
+            sprites[image_file.replace(".png", "") + "_left"] = flip(sprites_list)
+        else:
+            sprites[image_file.replace(".png", "")] = sprites_list
+
+    return sprites
+
+
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "NinjaFrog", 32, 32, direction=True)
+    ANIMATION_DELAY = 3
 
     def __init__(self, x, y, width, height):
         # initialize the super class
@@ -26,6 +60,8 @@ class Player(pygame.sprite.Sprite):
 
         self.direction = "left"
         self.animation_count = 0
+
+        self.mask = None
 
         self.fall_count = 0
 
@@ -48,13 +84,39 @@ class Player(pygame.sprite.Sprite):
     def loop(self, fps):
         """Called on every game loop tick; moves the player"""
         # apply gravity
-        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
-        self.fall_count += 1
-
+        # self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
+        self.fall_count += 1
+        self.update_sprite()
+
+    def update_sprite(self):
+        sprite_sheet_base = "idle"
+
+        # if moving, use "run" sprites
+        if self.x_vel != 0:
+            sprite_sheet_base = "run"
+
+        # load direction-specific sprites
+        sprite_sheet = sprite_sheet_base + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet]
+
+        # rotate through sprites to animate
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+
+        self.update_rect_and_mask()
+
+    def update_rect_and_mask(self):
+        # resize the rectangle to match the current sprite; keep same position
+        self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
+        # set mask for more precise collision detection
+        # see https://www.pygame.org/docs/ref/sprite.html#pygame.sprite.collide_mask
+        self.mask = pygame.mask.from_surface(self.sprite)
+
     def draw(self, window):
-        pygame.draw.rect(window, self.COLOR, self.rect)
+        window.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 def get_background(name):
